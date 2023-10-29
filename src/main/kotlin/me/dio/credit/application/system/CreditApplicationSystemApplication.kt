af7dfa58a49591a2487a76ef9489812c0b7cd518
Cassio Menezes <cassio.menezes@outlook.com.br>
2023-10-29 13:22:47 -0300

    doc: apresentacao da aplicacao

--- src/main/kotlin/me/dio/credit/application/system/CreditApplicationSystemApplication.kt
+++ src/main/kotlin/me/dio/credit/application/system/CreditApplicationSystemApplication.kt
@@ -3,6 +3,16 @@ package me.dio.credit.application.system
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * API para sistema de avaliacao de credito em Kotlin usando Spring Framework
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ *
+ * @author cami-la
+ * @author cassioborgesmenezes
+ */
 @SpringBootApplication
 class CreditApplicationSystemApplication
 
